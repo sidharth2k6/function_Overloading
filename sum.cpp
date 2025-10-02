656d99ac3ccfb97600f8908c77bce4f3ac305dbf
Sidharth <sidharthacker@gmail.com>
2025-10-02 19:49:04 +0530

    modify sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 class Overloading{
 public:
+    void printline(char ch = '*', int n = 10){
+        int i;
+        for(i = 0; i < n; i++){
+            cout << ch;
+        }
+        cout << endl;
+    }
     void sum(int a, int b){
         cout << "Sum of " << a << " + " << b << " = " << a + b << endl;
     }
@@ -24,5 +31,8 @@ int main(){
     obj.sum(3, 4.3);
     obj.sum(3.4, 4);
     obj.sum(3.4, 4.3);
+    obj.printline();
+    obj.printline('-');
+    obj.printline('+', 5);
     return 0;
 }
